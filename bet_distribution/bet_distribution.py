3f840fb224537792344b64cc79ea01e0d5ed3411
Martin Kunz <martinkunz@email.cz>
2020-11-10 01:31:01 +0100

    bet_distribution to class Bet_distribution
    This is the final state of Bet_distribution except documentation.
    TODO: documentation <10-11-20, kunzaatko> #

--- bet_distribution/bet_distribution.py
+++ bet_distribution/bet_distribution.py
@@ -1,38 +1,53 @@
 import numpy as np
 import pandas as pd
 
-def inc_bet_distribution(bankroll, P_dis, summary, opps):
-    min_bet = summary.iloc[0].to_dict()['Min_bet']
-    max_bet = summary.iloc[0].to_dict()['Max_bet']
-    P_dis_arr = P_dis.to_numpy # assuming we stick to the suggested data structure as in the input
-    odds = opps.to_numpy[7:9,:] # ['OddsH', 'OddsD', 'OddsA']
+class Bet_distribution:
+    def __init__(self):
+        self.summary = None
+        self.bets = None
+        self.odds = None
+        self.P_dis = None
+        self.bet_exp_profit_margin = 1.05 # TODO: zkusit různé hodnoty abychom započítaly nepřesnost našeho modelu <08-11-20, kunzaatko> #
 
-    # předpokládaný zisk ze sázky jednoho kreditu
-    exp_profit_per_cell = P_dis_arr * odds # they should be the same shape
+    def eval_summary(self, summary):
+        self.summary = {'min_bet': summary.at[0, 'min_bet'], 'max_bet': summary.at[0, 'max_bet'], 'Bankroll': summary.at[0, 'Bankroll'] }
 
-    bets = np.zeros(P_dis_arr.shape()) # underlining structure to count the ideal bets
-    purse = bankroll # kolik máme k dispozici kreditů k sázení
-    exp_profit = 0 # pro testování
-    while purse > (min_bet+max_bet):
-        if exp_profit_per_cell.max() > 1.00: # TODO: zkusit různé hodnoty abychom započítaly nepřesnost našeho modelu <08-11-20, kunzaatko> #
-            best_bets = np.argmax(exp_profit)
-            # for je tu pro případ více cellů se stejným předpokládaným profitem
-            for best_bet in best_bets:
-                # máme dost na vsazení maximální sázky
-                if purse >= max_bet:
-                    bets[best_bet] = max_bet
-                    exp_profit += max_bet * bets[best_bet]
-                    purse -= max_bet
-                elif purse >= min_bet:
-                    bets[best_bet] = purse
-                    exp_profit += max_bet * bets[best_bet]
-                    purse -= purse
-                    break
-                else: # nemáme na minimální sázku
-                    break
-            exp_profit[best_bets] = 0 # abychom mohli najít další nejvyšší předpokládané zisky, vynulujeme zisky, na které už jsme vsadili
-        else:
-            break # pokud už není žádný cell, pro který bychom předpokládali zisk
+    def eval_opps(self, opps):
+        self.odds = opps[['OddsH', 'OddsD', 'OddsA']].sort_index()
+        self.bets = pd.DataFrame(data=np.zeros([3,len(opps)]), columns=['BetH', 'BetD', 'BetA'], index=self.odds.index)
 
-    return pd.DataFrame(data=bets, columns=['BetH', 'BetD', 'BetA'], index=opps.index)
+    def eval_P_dis(self, P_dis):
+        self.P_dis = P_dis.sort_index()
+
+    def update_bets(self):
+        # předpokládaný zisk na jeden vsazený kredit
+        exp_profit = pd.DataFrame(data=(self.odds.to_numpy() * self.P_dis.to_numpy()), columns=["ExpH","ExpD","ExpA"], index=self.P_dis.index) # index sorted so we multiply matching elements
+
+        # TODO: Tohle není deterministicky nejlepší možnost, ale počítá se vsazením co nejvíce peněz. <10-11-20, kunzaatko>
+        while exp_profit.max() >= self.bet_exp_profit_margin:
+            argmax = np.unravel_index(exp_profit.to_numpy().argmax(), exp_profit.to_numpy().shape)
+            if self.summary['Bankroll'] >= (self.summary['min_bet'] + self.summary['max_bet']):
+                bet = self.summary['max_bet']
+                self.bets.iloc[argmax] = bet
+                exp_profit.iloc[argmax] = 0 # abychom mohli najít další nejvyšší předpokládané zisky, vynulujeme zisky, na které už jsme vsadili
+                self.summary['Bankroll'] -= bet
+
+            elif self.summary['Bankroll'] >= 2*self.summary['min_bet']:
+                bet = self.summary['Bankroll'] - self.summary['min_bet']
+                self.bets.iloc[argmax] = bet
+                exp_profit.iloc[argmax] = 0
+                self.summary['Bankroll'] -= bet
+
+            else:
+                bet = self.summary['Bankroll']
+                self.bets.iloc[argmax] = bet
+                exp_profit.iloc[argmax] = 0
+                self.summary['Bankroll'] -= bet
+                break
+
+    def run_iter(self, summary, opps, P_dis):
+        self.eval_summary(summary)
+        self.eval_opps(opps)
+        self.eval_P_dis(P_dis)
+        self.update_bets()
 

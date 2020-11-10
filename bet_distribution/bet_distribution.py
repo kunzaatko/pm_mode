1f9db4f599830113e1d94f6f5ca542639cfecdd1
Martin Kunz <martinkunz@email.cz>
2020-11-10 01:49:39 +0100

    Added documentation

--- bet_distribution/bet_distribution.py
+++ bet_distribution/bet_distribution.py
@@ -10,16 +10,40 @@ class Bet_distribution:
         self.bet_exp_profit_margin = 1.05 # TODO: zkusit různé hodnoty abychom započítaly nepřesnost našeho modelu <08-11-20, kunzaatko> #
 
     def eval_summary(self, summary):
+        '''
+        Update `self.summary`.
+
+        Parameters:
+            summary(pandas.DataFrame):  Summary of the current iteration. (from env)
+                                        Has to include 'Bankroll', 'max_bet' and 'min_bet'.
+        '''
         self.summary = {'min_bet': summary.at[0, 'min_bet'], 'max_bet': summary.at[0, 'max_bet'], 'Bankroll': summary.at[0, 'Bankroll'] }
 
     def eval_opps(self, opps):
+        '''
+        Update `self.odds` and `self.bets`.
+
+        Parameters:
+            opps(pandas.DataFrame): Opportunities for bets. (from env)
+                                    Has to include 'OddsH','OddsD' and 'OddsA'.
+        '''
         self.odds = opps[['OddsH', 'OddsD', 'OddsA']].sort_index()
         self.bets = pd.DataFrame(data=np.zeros([3,len(opps)]), columns=['BetH', 'BetD', 'BetA'], index=self.odds.index)
 
     def eval_P_dis(self, P_dis):
+        '''
+        Update `self.P_dis`
+
+        Parameters:
+            P_dis(pandas.DataFrame):    P_dis from model.
+                                        Should include 'P(H)', 'P(D)' and 'P(A)'.
+        '''
         self.P_dis = P_dis.sort_index()
 
     def update_bets(self):
+        '''
+        Place optimal bets based `self.P_dis` and `self.odds`.
+        '''
         # předpokládaný zisk na jeden vsazený kredit
         exp_profit = pd.DataFrame(data=(self.odds.to_numpy() * self.P_dis.to_numpy()), columns=["ExpH","ExpD","ExpA"], index=self.P_dis.index) # index sorted so we multiply matching elements
 
@@ -46,6 +70,17 @@ class Bet_distribution:
                 break
 
     def run_iter(self, summary, opps, P_dis):
+        '''
+        The outermost API for Bet_distribution. Run bet_distribution on the iter.
+
+        Parameters:
+            summary(pandas.DataFrame):  Summary of the current iteration. (from env)
+                                        Has to include 'Bankroll', 'max_bet' and 'min_bet'.
+            opps(pandas.DataFrame): Opportunities for bets. (from env)
+                                    Has to include 'OddsH','OddsD' and 'OddsA'.
+            P_dis(pandas.DataFrame):    P_dis from model.
+                                        Should include 'P(H)', 'P(D)' and 'P(A)'.
+        '''
         self.eval_summary(summary)
         self.eval_opps(opps)
         self.eval_P_dis(P_dis)

2f117d5e5f71bd312371f29ce713fcee74709d37
Martin Kunz <martinkunz@email.cz>
2020-11-16 00:27:31 +0100

    globals() instead locals()

--- scripts/init_feature_extraction.py
+++ scripts/init_feature_extraction.py
@@ -6,7 +6,7 @@ sys.path.append('..')
 from hackathon.src.environment import Environment
 from models.feature_extraction.feature_extraction import Data
 
-if not 'limits' in locals():
+if 'limits' not in globals():
     limits = {'start':'2000-03-20','end':'2011-06-30'}
 
 dataset = pd.read_csv('./hackathon/data/training_data.csv', parse_dates=['Date', 'Open'])

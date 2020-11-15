31ab7d1dc6cda682237dde3d798e42f58e4e7950
Martin Kunz <martinkunz@email.cz>
2020-11-16 00:03:09 +0100

    Adding read from local variable to script for faster limit set for debug

--- scripts/init_feature_extraction.py
+++ scripts/init_feature_extraction.py
@@ -6,9 +6,12 @@ sys.path.append('..')
 from hackathon.src.environment import Environment
 from models.feature_extraction.feature_extraction import Data
 
+if not limits in locals():
+    limits = {'start':'2000-03-20','end':'2011-06-30'}
+
 dataset = pd.read_csv('./hackathon/data/training_data.csv', parse_dates=['Date', 'Open'])
 env = Environment(dataset, None)
 data = Data()
-inc = [env.get_incremental_data(date) for date in pd.date_range(start='2000-03-20', end='2011-06-30')]
-opps = [env.get_opps(date) for date in pd.date_range(start='2000-03-20', end='2011-06-30')]
-summary = [env.generate_summary(date) for date in pd.date_range(start='2000-03-20', end='2011-06-30')]
+inc = [env.get_incremental_data(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]
+opps = [env.get_opps(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]
+summary = [env.generate_summary(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]

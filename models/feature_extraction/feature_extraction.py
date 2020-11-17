a69659ccf4d04d73fe51b94a5564ed613a3bd0bc
Martin Kunz <martinkunz@email.cz>
2020-11-17 14:57:02 +0100

    Changes in structure for better understanding

--- models/feature_extraction/feature_extraction.py
+++ models/feature_extraction/feature_extraction.py
@@ -65,11 +65,11 @@ class Data:
 
 
         # `self.match_data`
-        # index   || 'MatchID' | 'Date'       | 'Oppo'      | 'Side'       | 'M_Goals_Scored' | 'M_Goals_Conceded' | 'M_Win'   | 'M_Draw'
-        # team ID || match ID  | date of play | opponent id | side of play | goals scored     | goals concede      | match win | match draw
+        # index   || 'MatchID' | 'Date'       | 'Oppo'      | 'Home'       | 'Away'       | 'M_Goals_Scored' | 'M_Goals_Conceded' | 'M_Win'   | 'M_Draw'
+        # team ID || match ID  | date of play | opponent id | team is home | team is away | goals scored     | goals concede      | match win | match draw
         #          | 'M_Lose'   | 'M_P(Win)'      | 'M_P(Draw)'      | 'M_P(Lose)'      | 'M_Accu'
         #          | match lose | model prob. win | model prob. draw | model prob. lose | model accuracy
-        self.match_data = pd.DataFrame(columns=['MatchID', 'Date' , 'Oppo', 'Side',  'M_Goals_Scored', 'M_Goals_Conceded', 'M_Win','M_Draw', 'M_Lose','M_P(Win)','M_P(Draw)', 'M_P(Lose)','M_Accu'])
+        self.match_data = pd.DataFrame(columns=['MatchID', 'Date' , 'Oppo', 'Home', 'Away',  'M_Goals_Scored', 'M_Goals_Conceded', 'M_Win','M_Draw', 'M_Lose','M_P(Win)','M_P(Draw)', 'M_P(Lose)','M_Accu'])
 
 
     ######################################
@@ -89,55 +89,55 @@ class Data:
         inc(pandas.DataFrame): includes the played matches with the scores for the model.
         '''
         if summary is not None:
-            self._eval_summary(summary)
+            self._EVAL_summary(summary)
 
         if inc is not None:
             inc = inc.loc[:,~inc.columns.str.match('Unnamed')] # removing the 'Unnamed: 0' column (memory saning) See: https://stackoverflow.com/questions/36519086/how-to-get-rid-of-unnamed-0-column-in-a-pandas-dataframe
             self._curr_inc_teams = np.unique(np.concatenate((inc['HID'].to_numpy(dtype='int64'),inc['AID'].to_numpy(dtype='int64'))))
-            self._eval_inc(inc)
+            self._EVAL_inc(inc)
 
         if opps is not None:
             opps = opps.loc[:,~opps.columns.str.match('Unnamed')] # removing the 'Unnamed: 0' column (memory saning) See: https://stackoverflow.com/questions/36519086/how-to-get-rid-of-unnamed-0-column-in-a-pandas-dataframe
             self._curr_opps_teams = np.unique(np.concatenate((opps['HID'].to_numpy(dtype='int64'),opps['AID'].to_numpy(dtype='int64'))))
             opps['opps_Date'] = self.today
-            self._eval_opps(opps)
+            self._EVAL_opps(opps)
 
         if P_dis is not None:
-            self._eval_P_dis(P_dis)
+            self._EVAL_P_dis(P_dis)
 
         if bets is not None:
-            self._eval_bets(bets)
+            self._EVAL_bets(bets)
 
         if self._sort_columns:
-            self.matches = self.matches[['opps_Date','Sea','Date','Open','LID','HID','AID','HSC','ASC','H','D','A','OddsH','OddsD','OddsA','BetH','BetD','BetA']]
-            self.team_index
+            self.matches = self.matches[['opps_Date','Sea','Date','Open','LID','HID','AID','HSC','ASC','H','D','A','OddsH','OddsD','OddsA','P(H)','P(D)', 'P(A)','BetH','BetD','BetA']]
+            self.team_index = self.team_index[['LID', 'LL_Goals_Scored','LL_Goals_Conceded','LL_Wins', 'LL_Draws', 'LL_Loses', 'LL_Played', 'LL_Accu']]
 
         # }}}
 
-    def _eval_summary(self, summary):
+    def _EVAL_summary(self, summary):
         # {{{
         self.today = summary['Date'][0]
         self.bankroll = summary['Bankroll'][0]
         # }}}
 
-    def _eval_inc(self, inc):
+    def _EVAL_inc(self, inc):
         # {{{
         self._eval_teams(inc, self._curr_inc_teams)
         self._eval_matches(inc)
         # }}}
 
-    def _eval_opps(self, opps):
+    def _EVAL_opps(self, opps):
         # {{{
         self._eval_teams(opps, self._curr_inc_teams)
         self._eval_matches(opps)
         # }}}
 
-    def _eval_P_dis(self, P_dis):
+    def _EVAL_P_dis(self, P_dis):
         # {{{
         self._eval_matches(P_dis)
         # }}}
 
-    def _eval_bets(self, bets):
+    def _EVAL_bets(self, bets):
         # {{{
         self._eval_matches(bets)
         # }}}
@@ -196,6 +196,8 @@ class Data:
 
             # see also (https://stackoverflow.com/questions/45062340/check-if-single-element-is-contained-in-numpy-array)}}}
 
+    # TODO: Probably does not work correctly for the bets. The bets should not be combined for the `opps` and the `inc` but only for the `bets` dataframe. <17-11-20, kunzaatko> #
+    # TODO: the 'opps_Date' is not working. The indexes should not be concatenated but appended for new matches if they do not have the same 'opps_Date'... (When they are not added on the same day) <17-11-20, kunzaatko> # -> the problem with this is though that we would have to groupby matchid to to access a match, and multiple MatchIDs would be the same in the dataframe -> We should consider adding a new frame with this data (or maybe the bets should be recorded as an associated series of the match... What is your oppinion/solution?
     def _eval_matches(self, data_frame):
         # {{{
         self.matches = self.matches.combine_first(data_frame)
@@ -209,34 +211,105 @@ class Data:
         '''
         Update the features for the data stored in `self`.
         '''
-        self._update_LL_time_features()
-        self._update_time_data()
-        self._update_season_time_features()
-        self._update_last_match_features()
+        self._UPDATE_team_index_features()
+        self._UPDATE_time_data_features()
+        self._UPDATE_match_data_features()
 
-    def _update_time_data(self):
+    def _UPDATE_team_index_features(self):
         '''
-        Populate the `time_data` attribute from `matches`.
+        Populate all the features from the frame `self.team_index`
         '''
-        # TODO: should be done incrementaly <17-11-20, kunzaatko> #
-        pass
-
-    def _update_LL_time_features(self):
         if self.today in self.matches['Date']:
+            # a dataframe of all the todays matches (matches that where played on `self.today`)
             matches_played_today = self.matches.groupby('Date').get_group(self.today)
         else:
             matches_played_today = None
-        self._update_LL_PLAYED(matches_played_today)
+        self._update_LL_Played(matches_played_today)
+        self._update_LL_Goals(matches_played_today)
+        self._update_LL_Res(matches_played_today)
+        self._update_LL_Accu(matches_played_today)
 
-    def _update_LL_PLAYED(self, matches_played_today):
+    def _update_LL_Played(self, matches_played_today):
         if matches_played_today is not None:
             teams_played = np.concatenate((matches_played_today['HID'].to_numpy(dtype='int64'),matches_played_today['AID'].to_numpy(dtype='int64')))
-            self.team_index['LL_PLAYED'].loc[teams_played] = self.team_index.reindex(teams_played)['LL_PLAYED'].apply(lambda row: pd.isnan(row) and 1 or row + 1)
+            self.team_index['LL_Played'].loc[teams_played] = self.team_index.reindex(teams_played)['LL_Played'].apply(lambda row: pd.isnan(row) and 1 or row + 1)
 
-    def _update_last_match_features(self):
+    def _update_LL_Goals(self, matches_played_today):
+        '''
+        Update 'LL_Goals_Scored' and 'LL_Goals_Conceded' of the frame `self.team_index`
+        '''
+        if matches_played_today is not None:
+            pass
+
+    def _update_LL_Res(self, matches_played_today):
+        '''
+        Update 'LL_Wins', 'LL_Draws' and 'LL_Loses' of the frame `self.team_index`
+        '''
+        if matches_played_today is not None:
+            pass
+
+    def _update_LL_Accu(self, matches_played_today):
+        '''
+        Update 'LL_Accu' of the frame `self.team_index`
+        '''
+        if matches_played_today is not None:
+            pass
+
+    def _UPDATE_time_data_features(self):
+        '''
+        Populate all the features of `self.time_data`
+        '''
+        # TODO: should be done incrementaly <17-11-20, kunzaatko> #
+        if self.today in self.matches['Date']:
+            # a dataframe of all the todays matches (matches that where played on `self.today`)
+            matches_played_today = self.matches.groupby('Date').get_group(self.today)
+        else:
+            matches_played_today = None
+
+        self._update_SL_Goals(matches_played_today)
+        self._update_SL_Res(matches_played_today)
+        self._update_SL_Played(matches_played_today)
+        self._update_SL_Accu(matches_played_today)
+
+    # TODO: Could be unified with `_update_LL_Goals` as `_update_Goals` but for different frames. <17-11-20, kunzaatko> #
+    def _update_SL_Goals(self, matches_played_today):
+        '''
+        Update 'LL_Wins', 'LL_Draws' and 'LL_Loses' of the frame `self.team_index`
+        '''
         pass
 
-    def _update_season_time_features(self):
+    # TODO: Could be unified with `_update_LL_Res` as `_update_Res` but for different frames. <17-11-20, kunzaatko> #
+    def _update_SL_Res(self, matches_played_today):
+        pass
+
+    # TODO: Could be unified with `_update_LL_Played` as `_update_Played` but for different frames. <17-11-20, kunzaatko> #
+    def _update_SL_Played(self, matches_played_today):
+        pass
+
+    # TODO: Could be unified with `_update_LL_Accu` as `_update_Accu` but for different frames. <17-11-20, kunzaatko> #
+    def _update_SL_Accu(self, matches_played_today):
+        '''
+        Update 'SL_Accu' of the frame `self.team_index`
+        '''
+        pass
+
+    def _UPDATE_match_data_features(self):
+        '''
+        Populate all the features of `self.match_data`
+        '''
+        if self.today in self.matches['Date']:
+            # a dataframe of all the todays matches (matches that where played on `self.today`)
+            matches_played_today = self.matches.groupby('Date').get_group(self.today)
+        else:
+            matches_played_today = None
+        # TODO: should be done incrementaly <17-11-20, kunzaatko> #
+        self._update_add_matches(matches_played_today)
+        pass
+
+    def _update_add_matches(self, matches_played_today):
+        '''
+        Add the matches that were played today. The fields 'MatchID', 'Date' == self.today, 'Oppo' == HID/AID, 'Home' & 'Away' (int 1/0), 'M_Goals_Scored' & 'M_Goals_Conceded' (int), 'M_Win' & 'M_Draw' & 'M_Lose' (int 1/0), 'M_P(Win)' & 'M_P(Draw)' & 'M_P(Lose)' (float), 'M_Accu' should be filled.
+        '''
         pass
 
 

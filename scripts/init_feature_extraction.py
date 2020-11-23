eb7e849647bc1502974ddff4842f886b85887311
Martin Kunz <martinkunz@email.cz>
2020-11-23 21:48:17 +0100

    ELO counting in Data class

--- scripts/init_feature_extraction.py
+++ scripts/init_feature_extraction.py
@@ -10,7 +10,7 @@ limits = {'start':'2000-03-20','end':'2011-06-30'}
 
 dataset = pd.read_csv('./hackathon/data/training_data.csv', parse_dates=['Date', 'Open'])
 env = Environment(dataset, None)
-data = Data()
+data = Data(optional_data_cols=['ELO_rating'])
 inc = [env.get_incremental_data(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]
 opps = [env.get_opps(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]
 summary = [env.generate_summary(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]

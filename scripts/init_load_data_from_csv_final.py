9524a12d916a2babd1a40419bfa8caf07e558660
Martin Kunz <martinkunz@email.cz>
2020-11-28 11:22:29 +0100

    Adding load from csv

---
+++ scripts/init_load_data_from_csv_final.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append('..')
+from models.feature_extraction.feature_extraction import Data
+
+data = Data()
+data.LL_data = pd.read_csv('./LL_data_final.csv',index_col=0)
+data.SL_data = pd.read_csv('./SL_data_final.csv',index_col=0)
+data.matches = pd.read_csv('./matches_final.csv', parse_dates=['Date', 'Open'],index_col=0)
+data.match_data = pd.read_csv('./match_data_final.csv', parse_dates=['Date'],index_col=0)

75ac7c297736bde3c11b4b2f887cb7d907a2ba47
Martin Kunz <kunzaatko@imac.local>
2020-11-28 10:02:11 +0100

    Chagning to final training data set

--- scripts/init_feature_extraction.py
+++ scripts/init_feature_extraction.py
@@ -8,7 +8,7 @@ from models.feature_extraction.feature_extraction import Data
 
 limits = {'start':'2000-03-20','end':'2011-06-30'}
 
-dataset = pd.read_csv('./hackathon/data/training_data.csv', parse_dates=['Date', 'Open'])
+dataset = pd.read_csv('./hackathon/data/training_data_finals.csv', parse_dates=['Date', 'Open'])
 env = Environment(dataset, None)
 data = Data(optional_data_cols=['ELO_rating'])
 inc = [env.get_incremental_data(date) for date in pd.date_range(start=limits['start'], end=limits['end'])]
